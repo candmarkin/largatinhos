3fa77e108b2f9cb7c6cfb39d92ed7b6bc98b4a41
unknown <marcoscandido180@gmail.com>
2025-06-02 11:30:43 -0300

    Minor changes

--- app/api/gatos/route.ts
+++ app/api/gatos/route.ts
@@ -1,13 +1,4 @@
 import { neon } from "@neondatabase/serverless";
-
-export const checkEnvironment = () => {
-    const base_url =
-      process.env.NODE_ENV === "development"
-        ? "http://localhost:3000"
-        : "https://largatinhos.vercel.app";
-  
-    return base_url;
-}
  
 export async function GET() {
     const sql = neon(`${process.env.DATABASE_URL}`);
