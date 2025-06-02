98e38057685e10f6e933f7c963e6d9e18f2c5be1
unknown <marcoscandido180@gmail.com>
2025-06-02 11:38:11 -0300

    Minor changes

--- app/gatos/page.tsx
+++ app/gatos/page.tsx
@@ -1,27 +1,24 @@
 import { checkEnvironment } from "@/app/checkenv";
-import dynamic from "next/dynamic"
+import dynamic from "next/dynamic";
 import React from "react";
 
-const DynamicComponent = dynamic(() =>
-  import('../components/divGato').then((mod) => mod.DivGato)
-)
- 
+// Explicitly type the props for DivGato
+type DivGatoProps = { gatoID: { id: string } };
 
+const DynamicComponent = dynamic<DivGatoProps>(
+  () => import('../components/divGato').then((mod) => mod.DivGato)
+);
 
+export default async function GatosPage() {
+  const result = await fetch(checkEnvironment().concat('/api/gatos'));
+  const gatos = await result.json();
 
-
-export default async function GatosPage(){
-
-  const result = await fetch(checkEnvironment().concat('/api/gatos'))
-  const gatos = await result.json()
-
-return (<div className="m-4 sm:m-6 grid grid-cols-2 sm:grid-cols-3 md:grid-cols-5">{gatos.map(async (gatoid: {id: string}) => {
-
-          return(
-            <DynamicComponent key={"DIV" + gatoid.id} gatoID={gatoid}/>
-          )
-        })}</div>
-)
-
+  return (
+    <div className="m-4 sm:m-6 grid grid-cols-2 sm:grid-cols-3 md:grid-cols-5">
+      {gatos.map((gatoid: { id: string }) => (
+        <DynamicComponent key={"DIV" + gatoid.id} gatoID={gatoid} />
+      ))}
+    </div>
+  );
 }
 
